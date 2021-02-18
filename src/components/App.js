b0565eee9d57943498e49ad3a495e0458667837e
smartman1234 <john.smith.dev0724@gmail.com>
2021-02-18 12:13:46 +0530

    display categories

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [categories, setCategories] = useState(null);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const data = (
+        await axios.get(
+          "https://www.themealdb.com/api/json/v1/1/categories.php"
+        )
+      ).data;
+      setCategories(data.categories);
+    };
+    fetchCategories();
+  }, []);
+
+  console.log(categories);
+
+  return (
+    <div className="App">
+      <h1>Categories</h1>
+      {categories &&
+        categories.map((cat) => {
+          return (
+            <div key={cat.idCategory}>
+              <img src={cat.strCategoryThumb} alt={cat.strCategory} />
+              <h2>{cat.strCategory}</h2>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+
+export default App;

bc84d758646b3afa49e5c904b92d5906f7be4f1b
smartman1234 <john.smith.dev0724@gmail.com>
2021-02-19 11:15:36 +0530

    add navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Landing from "./Landing";
 import { Switch, Route } from "react-router-dom";
 import Category from "./Category";
@@ -6,28 +5,95 @@ import Recipe from "./Recipe";
 import SearchBar from "./SearchBar";
 import SearchRecipe from "./SearchRecipe";
 
-function App() {
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Fab from "@material-ui/core/Fab";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import Zoom from "@material-ui/core/Zoom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "fixed",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+function ScrollTop(props) {
+  const { children, window } = props;
+  const classes = useStyles();
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      "#back-to-top-anchor"
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+  };
+
   return (
-    <div className="App">
-      <SearchBar />
-      <Switch>
-        <Route path="/" exact>
-          <Landing />
-        </Route>
-        <Route path="/category/:id" exact>
-          <Category />
-        </Route>
-        <Route path="/recipe/:id" exact>
-          <Recipe />
-        </Route>
-        <Route path="/search/:query" exact>
-          <SearchRecipe />
-        </Route>
-        <Route>
-          <h1>404</h1>
-        </Route>
-      </Switch>
-    </div>
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+}
+
+function App(props) {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6">The Meal Hub</Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar id="back-to-top-anchor" />
+      <Container>
+        <Box my={2}>
+          <SearchBar />
+          <Switch>
+            <Route path="/" exact>
+              <Landing />
+            </Route>
+            <Route path="/category/:id" exact>
+              <Category />
+            </Route>
+            <Route path="/recipe/:id" exact>
+              <Recipe />
+            </Route>
+            <Route path="/search/:query" exact>
+              <SearchRecipe />
+            </Route>
+            <Route>
+              <h1>404</h1>
+            </Route>
+          </Switch>
+        </Box>
+      </Container>
+      <ScrollTop {...props}>
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </>
   );
 }
 
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
-import "./App.css";
 
 function Landing() {
   const history = useHistory();

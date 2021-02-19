aec4a5f44c987e9dc7b115c37feae18d042a07e8
smartman1234 <john.smith.dev0724@gmail.com>
2021-02-19 12:03:19 +0530

    style recipe card

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,18 +1,45 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
 function RecipeCard({ recipe }) {
   const history = useHistory();
+  const classes = useStyles();
   return (
-    <div
-      className="clickable"
+    <Card
+      className={classes.root}
       onClick={() => {
         history.push(`/recipe/${recipe.idMeal}`);
       }}
     >
-      <img src={recipe.strMealThumb + "/preview"} alt={recipe.strMeal} />
-      <h2>{recipe.strMeal}</h2>
-    </div>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={recipe.strMealThumb + "/preview"}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {recipe.strMeal}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 }
 

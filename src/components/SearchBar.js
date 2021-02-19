f1b629aca6f108b24eee60f900eaec3d7a2e8fd1
smartman1234 <john.smith.dev0724@gmail.com>
2021-02-19 11:24:40 +0530

    style search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,32 @@
 import { useRef } from "react";
 import { useHistory } from "react-router-dom";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: "2px 6px",
+    display: "flex",
+    alignItems: "center",
+    width: 800,
+    maxWidth: "100%",
+    margin: "auto",
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+}));
+
 function SearchBar() {
+  const classes = useStyles();
   const inputRef = useRef();
   const history = useHistory();
 
@@ -11,15 +36,21 @@ function SearchBar() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        ref={inputRef}
-        type="text"
+    <Paper onSubmit={handleSubmit} component="form" className={classes.root}>
+      <InputBase
+        inputRef={inputRef}
+        className={classes.input}
         placeholder="search recipe by text"
-        autoComplete="false"
+        inputProps={{ "aria-label": "search google maps" }}
       />
-      <button type="submit">Search</button>
-    </form>
+      <IconButton
+        type="submit"
+        className={classes.iconButton}
+        aria-label="search"
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   );
 }
 

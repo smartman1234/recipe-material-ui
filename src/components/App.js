be210f5b93227d9e76396436c6614582cd2e6f0e
smartman1234 <john.smith.dev0724@gmail.com>
2021-02-20 19:53:01 +0530

    add dark mode

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Category from "./Category";
 import Recipe from "./Recipe";
 import SearchBar from "./SearchBar";
 import SearchRecipe from "./SearchRecipe";
+import useLocalStorageState from "../hooks/useLocalStorageState";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -16,6 +17,10 @@ import Container from "@material-ui/core/Container";
 import Fab from "@material-ui/core/Fab";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import Zoom from "@material-ui/core/Zoom";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
+import IconButton from "@material-ui/core/IconButton";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 function ScrollTop(props) {
   const useStyles = makeStyles((theme) => ({
@@ -56,44 +61,71 @@ function ScrollTop(props) {
 }
 
 function App(props) {
+  const [darkState, setDarkState] = useLocalStorageState("darkMode", false);
+
+  const paletteType = darkState ? "dark" : "light";
+
+  const theme = createMuiTheme({
+    palette: {
+      type: paletteType,
+    },
+  });
+
+  const handleThemeChange = () => {
+    setDarkState(!darkState);
+  };
+
   return (
     <>
       <CssBaseline />
-      <AppBar>
-        <Toolbar>
-          <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
-            <Typography variant="h6">The Meal Hub</Typography>
-          </Link>
-        </Toolbar>
-      </AppBar>
-      <Toolbar id="back-to-top-anchor" />
-      <Container>
-        <Box my={2}>
-          <SearchBar />
-          <Switch>
-            <Route path="/" exact>
-              <Landing />
-            </Route>
-            <Route path="/category/:id" exact>
-              <Category />
-            </Route>
-            <Route path="/recipe/:id" exact>
-              <Recipe />
-            </Route>
-            <Route path="/search/:query" exact>
-              <SearchRecipe />
-            </Route>
-            <Route>
-              <h1>404</h1>
-            </Route>
-          </Switch>
-        </Box>
-      </Container>
-      <ScrollTop {...props}>
-        <Fab color="secondary" size="small" aria-label="scroll back to top">
-          <KeyboardArrowUpIcon />
-        </Fab>
-      </ScrollTop>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar>
+          <Toolbar>
+            <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+              <Typography variant="h6">The Meal Hub</Typography>
+            </Link>
+            <IconButton
+              style={{ margin: "auto", marginRight: "0" }}
+              aria-label="toggle dark mode"
+              aria-controls="menu-appbar"
+              aria-haspopup="false"
+              onClick={handleThemeChange}
+              color="inherit"
+            >
+              {darkState ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Toolbar id="back-to-top-anchor" />
+        <Container>
+          <Box my={2}>
+            <SearchBar />
+            <Switch>
+              <Route path="/" exact>
+                <Landing />
+              </Route>
+              <Route path="/category/:id" exact>
+                <Category />
+              </Route>
+              <Route path="/recipe/:id" exact>
+                <Recipe />
+              </Route>
+              <Route path="/search/:query" exact>
+                <SearchRecipe />
+              </Route>
+              <Route>
+                <h1>404</h1>
+              </Route>
+            </Switch>
+          </Box>
+        </Container>
+        <ScrollTop {...props}>
+          <Fab color="secondary" size="small" aria-label="scroll back to top">
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </ScrollTop>
+      </ThemeProvider>
     </>
   );
 }
